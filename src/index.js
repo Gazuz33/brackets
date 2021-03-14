a51bebb54089c21f4826545c4a0fd633186b268f
Andrew Goncharov <71984230+Gazuz33@users.noreply.github.com>
2021-03-14 17:59:28 +0300

    feat: add brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ var brack = [ ];
+    bracketsConfig.forEach(element => {
+    brack.push(element.join('')); });
+   for(var i = 0; i < brack.length; i++) {
+    if (str.includes(brack[i] ) ) {
+      str = str.replace(brack[i], "");
+      i = -1;
+    }
+  }
+    if (str.length === 0) return true;
+    else return false;
 }
